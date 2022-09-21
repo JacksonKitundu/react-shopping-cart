6a8cca924de7dc09204eb6e15dac92908196624d
jacksonKitundu <leopordjackson@gmail.com>
2022-09-21 11:29:06 +0300

    finalized dark mode and changed the ui a bit

--- reactapp/src/components/Card.js
+++ reactapp/src/components/Card.js
@@ -1,26 +1,42 @@
 import React from "react";
 
 export default function Card(props) {
-
-  const style = {
-    backgroundColor: props.prodName === "hoodie purple" ? "#cfb6f6" 
-    : props.prodName === "shirt pink" ? "#fdebe9"
-    : props.prodName === "short pink" ? "#fdebe9"
-    : props.prodName === "pant purple" ? "#cfb6f6" : ""
+   function changeStyle() {
+     let style
+    if (props.darkStatus) {
+      return style = {backgroundColor: "#00000b"}
+    }else {
+      return style = {
+        backgroundColor:
+          props.prodName === "hoodie purple"
+            ? "#cfb6f6"
+            : props.prodName === "shirt pink"
+            ? "#fdebe9"
+            : props.prodName === "short pink"
+            ? "#fdebe9"
+            : props.prodName === "pant purple"
+            ? "#cfb6f6"
+            : ""
+      };
+    }
   }
-  
+
+  const namePriceStyle = {
+    color: props.darkStatus ? "#f1f1f1" : "",
+  };
+
   return (
     <div className="card">
-      <img style={style} className="cardImg" src={props.image} alt="" />
+      <img style={changeStyle()} className="cardImg" src={props.image} alt="" />
       <div className="cardContent">
-        <div className="namePrice">
-        <h4>{props.price}</h4>
-        <h5>{props.prodName}</h5>
+        <div style={namePriceStyle} className="namePrice">
+          <h4>{props.price}</h4>
+          <h5>{props.prodName}</h5>
         </div>
         <div className="btnAdd">
           <button onClick={props.addToCart}>+</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- reactapp/src/components/Cart.js
+++ reactapp/src/components/Cart.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 export default function Cart(props) {
+  //darkMode
+  const styleCartItem = {
+    backgroundColor: props.darkStatus ? "#cfb6f636" : "",
+    borderLeft: "3px solid #bbbbbb"
+  }
+  const styleCartBtn = {
+    backgroundColor: props.darkStatus ? "#12082a" : "",
+    color: "#bbbbbb"
+  }
+
   return (
-      <div className="cartItem">
+      <div style={styleCartItem} className="cartItem">
         <div className="imgDesc">
         <div className="img"><img src={props.image} alt="" /></div>
         <div className="desc">
@@ -14,8 +24,8 @@ export default function Cart(props) {
         </div>
         </div>
         <div className="btnAdd">
-          <button onClick={props.increaseCount} className="btnOne">+</button>
-          <button onClick={props.decreaseCount} className="btnTwo">-</button>
+          <button style={styleCartBtn} onClick={props.increaseCount} className="btnOne">+</button>
+          <button style={styleCartBtn} onClick={props.decreaseCount} className="btnTwo">-</button>
         </div>
       </div>
   )
